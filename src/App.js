47c4f4647b2f683808407989b8dd6170393e063e
alamin-7 <alamin188914@gmail.com>
2024-05-27 01:31:54 +0600

    Routes added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
+import { Routes } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {Home, Profile, Signup, Login} from  '/components';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-    </Switch>
+      <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   </Router>
   );
 }
